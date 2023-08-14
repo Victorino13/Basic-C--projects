9ef0c7ad93a56473437d3c43c9cc59cb12ea5cce
Victorino13 <114184082+Victorino13@users.noreply.github.com>
2023-08-14 05:29:00 -0700

    PolymorpismAssignmentSubmission
    added to AbstractClassAssignment

--- AbstractClassAssignment/AbstractClassAssignment/Employee.cs
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -7,13 +7,19 @@ using System.Threading.Tasks;
 namespace AbstractClassAssignment
 {
     //Create another class, "Employee" and have it inherit from the Person class
-    public class Employee : Person
+    public class Employee : Person, IQuittable
     {
         public int Id { get; set; }
         //Implement the SayName() method inside of the Employee Class
         public override void SayName()
         {
             Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+        //Have your Employee class from the previous drill inherit and call the Quit()
+        //method on it. 
+        public void Quit(Employee employee)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ AbstractClassAssignment/AbstractClassAssignment/IQuittable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    //Create an interface called IQuittable and have it define
+    //a method called Quit().
+    interface IQuittable
+    {
+        void Quit(Employee employee);
+    }
+}
