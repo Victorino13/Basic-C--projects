c58f6517f748872de2d767bfd3d4db4d6d09ba6f
Victorino13 <114184082+Victorino13@users.noreply.github.com>
2023-08-16 02:10:58 -0700

    OperatorsAssignmentSubmission

---
+++ OperatorAssignmentSubmission/OperatorAssignmentSubmission/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorAssignmentSubmission
+{
+    //Create Employee class with Id, FirstName and LastName properties
+    public class Employee
+    {
+        public string empA, empB;
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator== (Employee empA, Employee empB)
+        {
+            return (empA.Id == empB.Id);
+        }
+        public static bool operator!= (Employee empA, Employee empB)
+        {
+            return (empA.Id != empB.Id);
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+    
+  
+    
+    
+}
---
+++ OperatorAssignmentSubmission/OperatorAssignmentSubmission/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorAssignmentSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //in the Program.cs file, instantiate two objects of the employee class and assign values
+            //to their properties. Then compare the two Employee objects using the newly overloaded operators
+            //and display the results.
+            Employee empA = new Employee();
+            empA.Id = "1001";
+            empA.FirstName = "Victorino";
+            empA.LastName = "Palacio";
+            
+            Employee empB = new Employee();
+            empB.Id = "1002";
+            empB.FirstName = "Jeff";
+            empB.LastName = "Quitevis";
+
+            Console.WriteLine(empA.Id != empB.Id);
+            Console.WriteLine(empA.Id == empB.Id);
+            Console.ReadLine();
+            
+            
+           
+
+        }
+    }
+}
