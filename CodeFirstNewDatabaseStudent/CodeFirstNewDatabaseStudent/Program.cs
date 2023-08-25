8da5809fe678eb2774a8911eb4d4d33ae319a6fd
Victorino13 <114184082+Victorino13@users.noreply.github.com>
2023-08-25 12:47:33 -0700

    CodeFirstDbStudent

---
+++ CodeFirstNewDatabaseStudent/CodeFirstNewDatabaseStudent/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstNewDatabaseStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                Console.WriteLine("Enter Student name: ");
+                var name = Console.ReadLine();
+
+                var student = new Student { StudentName = name };
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                var query = from b in db.Students
+                            orderby b.StudentName
+                            select b;
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.StudentName);
+                }
+            }
+        }
+    }
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        
+        public virtual List<Subject> Subjects { get; set; }
+    }
+
+    public class Subject
+    {
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+        
+        public int StudentId { get; set; }
+        public virtual Student Student { get; set; }
+    }
+
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -4,6 +4,41 @@ These are some small projects I made using C# and .Net Framework
 # Projects
 
 <a href="https://github.com/Victorino13/Basic-C--projects/tree/main/CarInsurance">•CarInsurance<br>
+
 This project is an MVC web application that mimics a car insurance website. It takes user's input
 on policy information and calculates a qoute based on business logic.
+
+<a href="https://github.com/Victorino13/Basic-C--projects/blob/main/MyFirstAzureWebApp">•My FIrst Azure Web Application<br>
+
+This is an empty web application project for the purpose of deploying in MS Azure.
+
+
+# Assignments
+
+•Abstract Class
+•Acme Accounting Systems
+•Boolean Logic
+•Branching
+•Calling methods
+•Class method
+•Console App Array
+•Console App Parts 1 to 6
+•Console Application 
+•Console Application strings and integers
+•Constructor
+•DateTime
+•Entity Framework Code First
+•Input
+•Lambda expression
+•Main Method
+•Math and comparison operators
+•Method
+•Method class 
+•Methods and Objects
+•Operator
+•Parsing enums
+•Try/Catch 
+
+
+
 
